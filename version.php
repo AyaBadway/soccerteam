a96eb823cca71bde58804f96c95c5747324fbe73
AyaBadway <ayaahmedbadawy@outlook.com>
2025-06-13 23:38:12 +0300

    add verion.php to specify plugin type / verion and moodle needed verion for install

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_soccerteam';     // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2025061300;             // Plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2022041900;             // Minimum Moodle version required (e.g., 4.0)
+$plugin->maturity  = MATURITY_STABLE;        // This is a stable release
+$plugin->release   = '1.0';                  // Human-readable version name
